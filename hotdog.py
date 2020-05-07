4a66b5d6bb0c2da6ec4695419d998acab0a94ec1
dheerenhdra <45158087+dheerenhdra@users.noreply.github.com>
2020-05-07 18:09:48 +0530

    problem- jack bought 400 hot dogs for the school picnic. if they were contained in packages of 8 hot dogs, how many total packages did he buy?

---
+++ hotdog.py
@@ -0,0 +1,9 @@
+totalHotDog = 400
+totalHotDogPerContainer = 8
+totalContainer = 8
+totalConatiner = 0
+while (totalHotDog >= totalHotDogPerContainer):
+    totalHotDog -= totalHotDogPerContainer
+    totalContainer += 1
+
+print("jack buy total", totalContainer," container")    
