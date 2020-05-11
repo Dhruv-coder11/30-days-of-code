8137d973499713ac8185e4bbdb27738d0b33e275
dheerenhdra <45158087+dheerenhdra@users.noreply.github.com>
2020-05-11 18:01:05 +0530

    time measurement of watching movies.

---
+++ timeM.py
@@ -0,0 +1,8 @@
+firstMovieTimeInMinute = 100
+secondMovieTimeInMinute = 110
+minutePerHour = 60
+
+# converion of minutes to hours
+
+spentTime = (float) (firstMovieTimeInMinute + secondMovieTimeInMinute)/minutePerHour
+print("total hours to watch movies",spentTime)
