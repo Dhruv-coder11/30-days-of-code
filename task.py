8ca621b86b79d90eba4e9893a60a0ea3f22dd4d3
dheerenhdra <45158087+dheerenhdra@users.noreply.github.com>
2020-05-18 16:52:11 +0530

    dictionary creation and find the value by the user input.

---
+++ task.py
@@ -0,0 +1,8 @@
+# create a dictionary and take input from the user and return the meaning of the word from the dictionary.
+
+D = {"dhruv":"14.01.2000",
+      "ram":"15.09.1998",
+      "shyam":"17.06.2001",
+      "dheerendra":"12.02.1995"}
+name = input("enter the name  :")
+print(D[name])
